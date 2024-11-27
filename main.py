b78d07daa0544d73e4bffc8af147fdd0b1c7e5c5
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2024-11-27 16:46:39 +0100

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim: ft=python ts=4 sw=4 sts=4 et fenc=utf-8
+# Original author: "Eivind Magnus Hvidevold" <hvidevold@gmail.com>
+# License: GNU GPLv3 at http://www.gnu.org/licenses/gpl.html
+
+'''
+'''
+
+import os
+import sys
+import re
+
+def main():
+    'entry point'
+
+if __name__ == '__main__':
+    main()
+
